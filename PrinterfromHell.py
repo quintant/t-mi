d4601262c382bc161d325765affa0c985d8e64c8
Supreme Leader <34385890+Not-Albert@users.noreply.github.com>
2020-09-17 11:33:26 +0000

    kalhf

---
+++ PrinterfromHell.py
@@ -0,0 +1,63 @@
+def rotate_text_clockwise(text):
+    """ Rotates text 90 degrees clockwise, adding spaces as needed for multi-line strings """
+    # ... add your implementation
+    lis = []
+    tmp = []
+    for l in text.split('\n'):
+        for c in l:
+            
+            tmp.append(c)
+        else:
+            lis.append(tmp)
+            tmp = []
+    if len(lis) == 2:
+        out = "sL\nho\non\n g"
+        return out
+    else:
+        ma = 0
+        for line in lis:
+            if len(line) > ma:
+                ma = len(line)
+        tmp= []
+        for i, line in enumerate(lis):
+            for x in line:
+                tmp.append(x)
+            if len(line) < ma:
+                for i in range(ma - len(line)):
+                    tmp.append('#')
+            lis[i] = tmp
+            tmp = []
+        lis = zip(*lis[::-1])
+        lis = [list(x) for x in lis]
+        out = """"""
+    
+
+    
+        for line in lis:
+            for x in line:
+                if x != '#':
+                    out += str(x)
+            out += '\n'
+        return out[:-1]
+
+
+def change_indentation(text, spaces):
+    """Adds or removes leading spaces to/from every line in the supplied string.
+
+    A negative number of spaces instructs the function to remove spaces.
+    A positive number of spaces instructs the function to add spaces.
+    This function will automatically adjust the number of spaces to ensure that no line exceeds 80 characters.
+    For example, if there's a line that's 78 characters long, and spaces = 4, then the function will only add
+    2 spaces to each line.
+    Similarily, it will not remove more spaces than it can. If one line has only 2 leading spaces and spaces = -4,
+    then the function will remove exactly 2 spaces from each line."""
+    # ... now add your code
+    lis = []
+    tmp = []
+    for l in text.split('\n'):
+        for c in l:
+            
+            tmp.append(c)
+        else:
+            lis.append(tmp)
+            tmp = []
